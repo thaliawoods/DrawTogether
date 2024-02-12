2cdbc1a56cd6029ae6e0577ceb4331d840cde8c1
thaliawoods <thaliadelano@gmail.com>
2024-02-12 18:46:53 +0100

    /dessiner/testhome

---
+++ DrawTogether/DrawTogether/DrawTogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawTogetherApp.swift
+//  DrawTogether
+//
+//  Created by Thalia Woods on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct DrawTogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
